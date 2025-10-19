c658a2f80a76efaee97a5f449d1b856350ed1701
Sofietche <126518025+Sofietche@users.noreply.github.com>
2025-10-19 19:37:53 +0200

    Revert "Auto-raise macOS file limit in npm start"

--- src/constants/colors.js
+++
@@ -1,1 +0,0 @@
-export const NEON_COLORS = ['#00E5FF', '#FF00C3', '#8C54FF', '#FF7A00'];
--- src/constants/layout.js
+++
@@ -1,6 +0,0 @@
-import { Dimensions } from 'react-native';
-
-const { width } = Dimensions.get('window');
-
-export const CARD_WIDTH = width * 0.9;
-export const CARD_HEIGHT = CARD_WIDTH * 1.4;
